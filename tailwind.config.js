c356523820541bd053a9cd5c4c67c8d5811becea
Serhii Trofimov <trofim.serhii@gmail.com>
2023-09-04 11:34:08 +0300

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+    fontFamily: {
+      main: "Comfortaa",
+    },
+  },
+  plugins: [],
+}
+
